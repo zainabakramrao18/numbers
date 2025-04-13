b7bc2d7e13e38e6676eb9015e4572dfccf2cf3b1
Zainab Akram Rao <zainabakramrao18@gmail.com>
2025-04-13 23:58:03 +0500

    Initial commit

---
+++ largest of 4.py
@@ -0,0 +1,11 @@
+# Get four numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+num4 = float(input("Enter fourth number: "))
+
+# Find the largest
+largest = max(num1, num2, num3, num4)
+
+# Print the result
+print("The largest number is:", largest)
